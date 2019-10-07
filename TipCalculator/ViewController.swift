c172ecf855cac039e8333abe4849637e46cf7979
Chris Baudouin, Jr <chrisbaudouinjr@icloud.com>
2019-10-06 23:00:36 -0400

    - Makes billTotalTextField first responder

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -15,16 +15,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var billTotalLabel: UITextField!
+    @IBOutlet weak var billTotalTextField: UITextField!
     let tipPercentages = [0.1, 0.15, 0.20]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        billTotalTextField.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func onBillEdit(_ sender: Any) {
-        let subtotal = Double(billTotalLabel.text!) ?? 0
+        let subtotal = Double(billTotalTextField.text!) ?? 0
         let tipAmount = subtotal * tipPercentages[tipControl.selectedSegmentIndex]
         let grandTotal = subtotal + tipAmount
         
